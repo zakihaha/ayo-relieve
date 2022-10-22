af6fa1a1ccbf12f7058df1e3e54731462730a650
zakihaha <muhzakich@students.amikom.ac.id>
2022-10-23 03:05:47 +0700

    get user from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+const getUser = () => {
+    const user = localStorage.getItem('user')
+    if (user) {
+        const userData = JSON.parse(user)
+        const element = document.createElement('p')
+        element.innerHTML = userData.name
+        element.classList.add('btn')
+        element.classList.add('btn-primary')
+        document.getElementById('user').appendChild(element)
+    }
+}
+
 const getCampaigns = async () => {
     try {
         const response = await fetch('https://634d0019acb391d34a90fbc0.mockapi.io/campaigns');
@@ -12,7 +24,7 @@ const getCampaigns = async () => {
                     <h3 class="mb-3">${campaign.title}</h3>
                     <div class="d-flex justify-content-between">
                         <p class="card-text">${campaign.company}</p>
-                        <a href="#" class="link-detail-campaign">Detail</a>
+                        <a href="detail.html?id=${campaign.id}" class="link-detail-campaign">Detail</a>
                     </div>
                 </div>
             </div>
@@ -24,4 +36,5 @@ const getCampaigns = async () => {
     }
 }
 
+getUser()
 getCampaigns()
